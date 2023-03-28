4c1d0f91a93c52b6cc740e2cf8d726d114feaefa
QGonz95 <q.gonz95@gmail.com>
2023-03-28 10:46:08 -0400

    show page completed, moving on to updated and delete

--- server.js
+++ server.js
@@ -11,20 +11,27 @@ app.get('/clients/new', (req, res) => {
 });
 // RESTful_ROUTE TO CREATE NEW CLIENT
 app.post('/clients/', (req, res) => {
-    // if (req.body.payment === 'on') {
-    //     req.body.payment = 'Paid in full'
-    // } else {
-    //     req.body.payment = 'Pending Payment'
-    // }
+    if (req.body.payment === 'on') {
+        req.body.payment = true
+    } else {
+        req.body.payment = false
+    }
     Client.create(req.body).then((err, addedClient)=>
         res.send(addedClient));
     });
 
-
+// RESTful_ROUTE TO INDEX PAGE 
+// since Mongoose update 7.0.3, call back function do not work, ASYNC functions work perfectly fine
 app.get('/clients', async (req, res) => {
     const clients = await Client.find({})
     res.render('index.ejs', {clients})
 });
+
+// RESTful_ROUTE TO INDEX PAGE 
+app.get('/clients/:id', async (req, res) => {
+    const client = await Client.findById(req.params.id)
+    res.render('show.ejs', { client })
+})
 
 app.listen(3000, () => {
     console.log('listening...');
